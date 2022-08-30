303d36c6df3933ebb4102c54b24c08c27688db6d
kim <rlarkdgh1056@naver.com>
2022-08-30 18:05:18 +0900

    Commit

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -1,14 +1,16 @@
-package hello.core.Order;
+package hello.core;
 
+import hello.core.Order.Order;
+import hello.core.Order.OrderService;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
